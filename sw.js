6de6cadec56bce163c5382379b3c17a2031d9778
achillesprojects07-bit <achilles.projects07@gmail.com>
2026-09-07 15:00:19 +0800

    Refresh cache for audited learning engine

--- sw.js
+++ sw.js
@@ -1,12 +1,10 @@
-const CACHE='american-accent-v3';
-const ASSETS=['./','./index.html','./css/app.css','./manifest.json','./content/curriculum.js','./content/day0.js','./content/day1.js','./content/stress-bank.js','./content/reduction-bank.js','./js/storage.js','./js/recording-engine.js','./js/lesson-engine.js','./js/app.js'];
+const CACHE='american-accent-v4';
+const ASSETS=['./','./index.html','./css/app.css','./manifest.json','./content/curriculum.js','./content/day0.js','./content/day1.js','./content/stress-bank.js','./content/reduction-bank.js','./js/storage.js','./js/audio-engine.js','./js/recording-engine.js','./js/lesson-engine.js','./js/app.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
   event.respondWith(fetch(event.request).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-    return response;
+    const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;
   }).catch(()=>caches.match(event.request)));
 });
