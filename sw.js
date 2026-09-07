705a356ec02a1c0a5d1530777952dce8cbe74835
achillesprojects07-bit <achilles.projects07@gmail.com>
2026-09-07 14:47:43 +0800

    Refresh PWA cache for recorder retry update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='american-accent-v2';
+const CACHE='american-accent-v3';
 const ASSETS=['./','./index.html','./css/app.css','./manifest.json','./content/curriculum.js','./content/day0.js','./content/day1.js','./content/stress-bank.js','./content/reduction-bank.js','./js/storage.js','./js/recording-engine.js','./js/lesson-engine.js','./js/app.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
